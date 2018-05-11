d2bb553304c007b963de08d4b47ef90eaba8fa9e
Damian Muca <damian_muca@epam.com>
2018-05-11 16:35:21 +0200

    Allow user to choose language

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
+import GameState.LocaleLanguages;
+
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        XOGame XOGame = new XOGame();
+        Locale language = loadLanguageIfInserted(args);
+        XOGame XOGame = new XOGame(language);
         XOGame.runGame();
+    }
+
+    private static Locale loadLanguageIfInserted(String[] args) {
+        return args.length == 0 ? LocaleLanguages.valueOf("").getLanguage() : LocaleLanguages.valueOf(args[0]).getLanguage();
     }
 }
--- src/main/java/XOGame.java
+++ src/main/java/XOGame.java
@@ -3,22 +3,22 @@ import Data.Messenger.MessageKeys;
 import Data.Messenger.Messenger;
 import GameState.GameController;
 import GameState.States.StartGameState;
+import GameState.UserIO.UserIO;
+
 import java.util.Locale;
 
 class XOGame {
     private GameController gameController;
     private Messenger messenger;
 
-    XOGame() {
-        Locale PL = new Locale("pl", "PL");
-        Locale ENG = Locale.ENGLISH;
-        messenger = new Messenger(PL, System.out::println);
+    XOGame(Locale language) {
+        messenger = new Messenger(language, System.out::println);
         showInfoAboutGameToTheUser();
         gameController = new StartGameState(messenger);
     }
 
     private void showInfoAboutGameToTheUser() {
-        messenger.print(MessageKeys.startInfoAboutGame, Colors.ACTIVE, Colors.DEFAULT);
+        messenger.print(MessageKeys.startInfoAboutGame, Colors.ACTIVE, Colors.DEFAULT, messenger.translateKey(MessageKeys.quit));
     }
 
     void runGame() {
