07aab823c7c410fb5e04d91dd22a28eae3aeb1e3
Damian Muca <damian_muca@epam.com>
2018-05-16 15:15:20 +0200

    Create tests for RoundStateDraw and RoundStateOWon

--- src/test/java/roundStateTest/RoundStateOWonTest.java
+++ src/test/java/roundStateTest/RoundStateOWonTest.java
@@ -1,9 +1,14 @@
 package roundStateTest;
 
+import data.Symbol;
+import data.players.Player;
+import data.players.Players;
 import data.round.roundState.*;
+import messenger.MessageKeys;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
@@ -43,5 +48,28 @@ public class RoundStateOWonTest {
     public void roundStateOWon_isSameStateAs_null_shouldReturnFalse() {
         boolean nullRoundStateEqualToUnfinishedFalse = roundStateOWon.equals(null);
         assertFalse(nullRoundStateEqualToUnfinishedFalse);
+    }
+
+    @Test
+    public void getMassageKey_returns_MessageKeys_theRoundWins() {
+        assertEquals(roundStateOWon.getMessageKey(), MessageKeys.theRoundWins);
+    }
+
+    @Test
+    public void addPointsAccordingRoundFinishedState_shouldReturn3to0 () {
+        Player playerX, playerO;
+        playerX = new Player("Koczkodan", Symbol.O);
+        playerO = new Player("Nie Koczkodan", Symbol.X);
+        Players players = new Players(playerO, playerX, Symbol.O);
+
+        roundStateOWon.addPointsAccordingRoundFinishedState(players);
+
+        assertEquals(playerO.getPlayerScores(), 3);
+        assertEquals(playerX.getPlayerScores(), 0);
+    }
+
+    @Test
+    public void getMessageKeyArguments_returnArgumentsForStringFormat_SymbolO() {
+        assertEquals(roundStateOWon.getMessageKeyArguments(), new Object[]{Symbol.O});
     }
 }
