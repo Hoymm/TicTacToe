68869cb29c19d897eb63541331fa4c9bad4a8ea9
Damian Muca <damian_muca@epam.com>
2018-05-16 15:18:20 +0200

    Cretate tests for ROundStateXWon

--- src/test/java/roundStateTest/RoundStateXWonTest.java
+++ src/test/java/roundStateTest/RoundStateXWonTest.java
@@ -1,12 +1,17 @@
 package roundStateTest;
 
+import data.Symbol;
+import data.players.Player;
+import data.players.Players;
 import data.round.roundState.RoundStateDraw;
 import data.round.roundState.RoundStateOWon;
 import data.round.roundState.RoundStateUnfinished;
 import data.round.roundState.RoundStateXWon;
+import messenger.MessageKeys;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
@@ -46,5 +51,28 @@ public class RoundStateXWonTest {
     public void roundStateXWon_isSameStateAs_null_shouldReturnFalse() {
         boolean nullRoundStateEqualToUnfinishedFalse = roundStateXWon.equals(null);
         assertFalse(nullRoundStateEqualToUnfinishedFalse);
+    }
+
+    @Test
+    public void getMassageKey_returns_MessageKeys_theRoundWins() {
+        assertEquals(roundStateXWon.getMessageKey(), MessageKeys.theRoundWins);
+    }
+
+    @Test
+    public void addPointsAccordingRoundFinishedState_shouldReturn3to0 () {
+        Player playerX, playerO;
+        playerX = new Player("Koczkodan", Symbol.O);
+        playerO = new Player("Nie Koczkodan", Symbol.X);
+        Players players = new Players(playerO, playerX, Symbol.O);
+
+        roundStateXWon.addPointsAccordingRoundFinishedState(players);
+
+        assertEquals(playerO.getPlayerScores(), 0);
+        assertEquals(playerX.getPlayerScores(), 3);
+    }
+
+    @Test
+    public void getMessageKeyArguments_returnArgumentsForStringFormat_SymbolX() {
+        assertEquals(roundStateXWon.getMessageKeyArguments(), new Object[]{Symbol.X});
     }
 }
