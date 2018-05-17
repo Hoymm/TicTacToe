1a2a2faa80b34e046fc3ecee5826c10a1cfa6d1d
Damian Muca <damian_muca@epam.com>
2018-05-17 09:40:59 +0200

    Code refactor

--- src/main/java/gameState/userIO/startSettingsInput/StartSettingsLoader.java
+++ src/main/java/gameState/userIO/startSettingsInput/StartSettingsLoader.java
@@ -25,13 +25,21 @@ public class StartSettingsLoader {
 
     private StartSettings loadStartDataFromUser() {
         StartSettings startSettings = new StartSettings();
-        startSettings = loadPlayerOName(startSettings);
-        startSettings = loadPlayerXName(startSettings);
-        startSettings = loadWhoStartFirst(startSettings);
-        startSettings = loadBoardWidth(startSettings);
-        startSettings = loadBoardHeight(startSettings);
-        startSettings = loadHowManySymbolsInLineToWinRound(startSettings);
+        loadPlayerNames(startSettings);
+        loadWhoStartFirst(startSettings);
+        loadBoardParams(startSettings);
         return startSettings;
+    }
+
+    private void loadPlayerNames(StartSettings startSettings) {
+        loadPlayerOName(startSettings);
+        loadPlayerXName(startSettings);
+    }
+
+    private void loadBoardParams(StartSettings startSettings) {
+        loadBoardWidth(startSettings);
+        loadBoardHeight(startSettings);
+        loadHowManySymbolsInLineToWinRound(startSettings);
     }
 
     public static StartSettings loadExampleData() {
@@ -45,14 +53,12 @@ public class StartSettingsLoader {
         return startSettings;
     }
 
-    private StartSettings loadPlayerOName(StartSettings startSettings) {
+    private void loadPlayerOName(StartSettings startSettings) {
         startSettings.namePlayerO = questionAndValidatePlayerName(Symbol.O.toString());
-        return startSettings;
     }
 
-    private StartSettings loadPlayerXName(StartSettings startSettings) {
+    private void loadPlayerXName(StartSettings startSettings) {
         startSettings.namePlayerX = questionAndValidatePlayerName(Symbol.X.toString());
-        return startSettings;
     }
 
     private String questionAndValidatePlayerName(String player) {
@@ -60,7 +66,7 @@ public class StartSettingsLoader {
         return userInputProvider.get().trim();
     }
 
-    private StartSettings loadWhoStartFirst(StartSettings startSettings) {
+    private void loadWhoStartFirst(StartSettings startSettings) {
         String symbolToPlayFirst = "";
         while(!startSettingsValidator.isValidSymbol(symbolToPlayFirst)){
             messenger.printf(MessageKeys.questionAboutStartingPlayer, Symbol.O, Symbol.X);
@@ -70,17 +76,14 @@ public class StartSettingsLoader {
             }
         }
         startSettings.whoStartsFirst = Symbol.valueOf(symbolToPlayFirst.trim());
-        return startSettings;
     }
 
-    private StartSettings loadBoardWidth(StartSettings startSettings) {
+    private void loadBoardWidth(StartSettings startSettings) {
         startSettings.boardWidth = loadAndValidateTableParam(messenger.translateKey(MessageKeys.width));
-        return startSettings;
     }
 
-    private StartSettings loadBoardHeight(StartSettings startSettings) {
+    private void loadBoardHeight(StartSettings startSettings) {
         startSettings.boardHeight = loadAndValidateTableParam(messenger.translateKey(MessageKeys.height));
-        return startSettings;
     }
 
     private int loadAndValidateTableParam(String widthOrHeight) {
@@ -97,10 +100,9 @@ public class StartSettingsLoader {
         return Integer.valueOf(tableParameterString);
     }
 
-    private StartSettings loadHowManySymbolsInLineToWinRound(StartSettings startSettings) {
+    private void loadHowManySymbolsInLineToWinRound(StartSettings startSettings) {
         int maxFromWidthAndHeightOfBoard = Math.max(startSettings.boardHeight, startSettings.boardWidth);
         startSettings.howManySymbolsInARowToWinRound = howManySymbolsInUnbrokenLineToWinRound(maxFromWidthAndHeightOfBoard);
-        return startSettings;
     }
 
     private int howManySymbolsInUnbrokenLineToWinRound(int maxFromBoardWidthAndHeight) {
